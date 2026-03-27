7b9141c140628bfae90c6f7596846b4c43aaaa8f
mikiyasu <S0330@MacBook-Air.local>
2026-03-27 12:10:15 +0900

    Posts機能のファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_27_024935) do
+ActiveRecord::Schema[7.2].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
