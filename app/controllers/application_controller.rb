b4e625488e5d6f52c2e523c740e6518e5c6e1de0
mikiyasu <S0330@MacBook-Air.local>
2026-04-01 12:58:03 +0900

    ログイン後のトップページ（仮）を作成、ログイン後遷移の設定を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,18 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || dashboard_path
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    root_path
+  end
+
+  protected
 
   def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+   devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
 end
