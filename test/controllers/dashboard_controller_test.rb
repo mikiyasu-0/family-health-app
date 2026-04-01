fa8deeeeaea866843aabf6f82168e4d817da36c1
mikiyasu <S0330@MacBook-Air.local>
2026-04-01 14:44:18 +0900

    dashboard controllerのtestでassert_responseをsuccessからredirectに変更しました

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get dashboard_index_url
-    assert_response :success
+  test "should redirect to sign in when not logged in" do
+    get dashboard_url
+    assert_response :redirect
+    assert_redirected_to new_user_session_url
   end
 end
