233747d1d47f20fce04bfef7010e8a5c8ee3eeac
mikiyasu <S0330@MacBook-Air.local>
2026-04-15 11:51:54 +0900

    exercise_records_controllerのedit,updateアクションを設定しました

--- app/controllers/exercise_records_controller.rb
+++ app/controllers/exercise_records_controller.rb
@@ -1,5 +1,6 @@
 class ExerciseRecordsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_exercise_record, only: %i[edit update]
 
   def create
     @exercise_record = current_user.exercise_records.new(exercise_record_params)
@@ -7,13 +8,32 @@ class ExerciseRecordsController < ApplicationController
     if @exercise_record.save
       redirect_to dashboard_path, notice: "運動を記録しました"
     else
-      redirect_to dashboard_path, alert: @exercise_record.errors.full_message.join(", ")
+      redirect_to dashboard_path, alert: @exercise_record.errors.full_messages.join(", ")
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @exercise_record.update(exercise_record_memo_params)
+      redirect_to dashboard_path, notice: "メモを更新しました"
+    else
+      redirect_to dashboard_path, alert: @exercise_record.errors.full_messages.join(", ")
     end
   end
 
   private
 
+  def set_exercise_record
+    @exercise_record = current_user.exercise_records.find(params[:id])
+  end
+
   def exercise_record_params
     params.require(:exercise_record).permit(:exercise_type)
+  end
+
+  def exercise_record_memo_params
+    params.require(:exercise_record).permit(:memo)
   end
 end
