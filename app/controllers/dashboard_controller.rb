68a76d06a8110a39ad6e5e9c03a0f3c21173931b
mikiyasu <S0330@MacBook-Air.local>
2026-04-15 17:03:42 +0900

    運動項目とmemoの表示・入力を設定して、保存されることを確認しました

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,9 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @exercise_types = ExerciseRecord::EXERCISE_TYPES
+    @today_exercise_records = current_user.exercise_records.where(
+      created_at: Time.current.beginning_of_day..Time.current.end_of_day
+    )
   end
 end
