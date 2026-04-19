f62215d65291ec20fe80eacfd964bb665d9ca0b0
mikiyasu <S0330@MacBook-Air.local>
2026-04-19 15:47:15 +0900

    マイグレーションファイルにカラムの追加、ユニーク制約を追加しました

---
+++ db/migrate/20260419063809_create_reactions.rb
@@ -0,0 +1,13 @@
+class CreateReactions < ActiveRecord::Migration[7.2]
+  def change
+    create_table :reactions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :exercise_record, null: false, foreign_key: true
+      t.string :reaction_type, null: false
+
+      t.timestamps
+    end
+
+    add_index :reactions, [:user_id, :exercise_record_id, :reaction_type], unique: true
+  end
+end
