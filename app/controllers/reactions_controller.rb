b104d372ac6d32a37b41d1aad4c2c514cc8c4243
mikiyasu <S0330@MacBook-Air.local>
2026-04-20 14:08:58 +0900

    reactions_controllerを生成してcreateアクションを設定しました

---
+++ app/controllers/reactions_controller.rb
@@ -0,0 +1,32 @@
+class ReactionsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @exercise_record = ExerciseRecord.find(prams[:exercise_record_id])
+    record_user == @exercise_record.user
+
+    if record_user == current_user
+      return redirect_to dashboard_path, alert: "この運動記録にはリアクションできません"
+    end
+
+    shared_group_exists = current_user.groups
+                                      .joins(:users)
+                                      .where(users: { id: record_user.id })
+                                      .exists?
+
+    unless shared_group_exists
+      return redirect_to dashboard_path, alert: "この運動記録にはリアクションできません"
+    end
+
+    reaction = current_user.reactions.create(
+      exercise_record: @exercise_record,
+      reaction_type: params[:reaction_type]
+    )
+
+    if reaction.persisted?
+      redirect_to dashboard_path, notice: "リアクションしました"
+    else
+      redirect_to dashboard_path, alert: "リアクションできませんでした"
+    end
+  end
+end
