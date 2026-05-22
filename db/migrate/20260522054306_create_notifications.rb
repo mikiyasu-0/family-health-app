569e2469e415db9d345ef53671d0e4bf214a2163
mikiyasu <S0330@MacBook-Air.local>
2026-05-22 14:46:14 +0900

    notificationモデルの雛形作成

---
+++ db/migrate/20260522054306_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration[7.2]
+  def change
+    create_table :notifications do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :notifiable, polymorphic: true, null: false
+      t.string :notification_type, null: false
+      t.datetime :read_at
+
+      t.timestamps
+    end
+  end
+end
