49b9b3995a98c5f45ddcea840c22a21c68e4b653
mikiyasu <S0330@MacBook-Air.local>
2026-05-27 16:26:51 +0900

    notifications_controller.rbを作成、indexアクションを編集、ヘッダーに通知一覧のリンクを用意する

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,9 @@
+class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @notifications = current_user.notifications
+                                 .preload(notifiable: :user)
+                                 .order(create_at: :desc)
+  end
+end
