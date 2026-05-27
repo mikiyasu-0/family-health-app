00f6eb44c89e485517d6c48cced645933c849eb6
mikiyasu <S0330@MacBook-Air.local>
2026-05-27 17:05:29 +0900

    テストの際に必要なログインユーザーを用意しました

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class NotificationsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = User.create!(
+      name: "通知受信者",
+      email: "recipient@example.com",
+      password: "password"
+    )
+
+    sign_in @user
+  end
+
   test "should get index" do
-    get notifications_index_url
+    get notifications_url
     assert_response :success
   end
 end
