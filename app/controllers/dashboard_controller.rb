8bbe89428b555a226db47be1d5a8320d8a81beba
mikiyasu <S0330@MacBook-Air.local>
2026-06-24 14:46:37 +0900

    週間サマリーを表示する

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,7 @@ class DashboardController < ApplicationController
     @current_streak = CurrentStreakCalculator.new(current_user).call
     @longest_streak = LongestStreakCalculator.new(current_user).call
     @streak_badge = StreakBadgeSelector.new(@current_streak).call
+    @weekly_summary = WeeklySummaryCalculator.new(current_user).call
 
     today_range = Time.current.beginning_of_day..Time.current.end_of_day
 
