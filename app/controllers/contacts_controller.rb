659332beffd9b0e4ed987520946cfccbf226f6cc
mikiyasu <S0330@MacBook-Air.local>
2026-08-05 13:07:58 +0900

    contact_controllerのcreateアクションを設定しました

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,21 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.valid?
+      redirect_to new_contact_path,
+                  notice: "お問い合わせ内容を確認しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
 end
