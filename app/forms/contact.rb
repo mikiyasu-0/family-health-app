616cb7a9d9b50a07df77a68d5f4ce41cbd17900b
mikiyasu <S0330@MacBook-Air.local>
2026-08-05 23:59:37 +0900

    テストエラーの修正を行いました

--- app/forms/contact.rb
+++ app/forms/contact.rb
@@ -6,12 +6,12 @@ class Contact
   validates :name,
             presence: true,
             length: { maximum: 50 }
-  
+
   validates :email,
             presence: true,
             format: { with: URI::MailTo::EMAIL_REGEXP }
 
   validates :message,
             presence: true,
-            length: { minimum:10, maximum: 1_000 }
+            length: { minimum: 10, maximum: 1_000 }
 end
--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -2,10 +2,24 @@ require "test_helper"
 
 class ContactMailerTest < ActionMailer::TestCase
   test "notify_admin" do
-    mail = ContactMailer.notify_admin
-    assert_equal "Notify admin", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    contact = Contact.new(
+      name: "テストユーザー",
+      email: "test@example.com",
+      message: "お問い合わせメールのテスト内容です。"
+    )
+
+    mail = ContactMailer.notify_admin(contact)
+
+    assert_equal(
+      "【ファミリーステップ】お問い合わせが届きました",
+      mail.subject
+    )
+    assert_equal [ENV.fetch("CONTACT_EMAIL")], mail.to
+    assert_equal ["test@example.com"], mail.reply_to
+
+    text_body = mail.text_part.body.decoded
+
+    assert_match "テストユーザー", text_body
+    assert_match "お問い合わせメールのテスト内容です。", text_body
   end
 end
